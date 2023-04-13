6ace24f6127f288688550f06f08256aca8bb114a
olex <olexandr.samus.2004@gmail.com>
2023-04-13 13:46:03 +0300

    add hw2

---
+++ hw2/task1.py
@@ -0,0 +1,22 @@
+# Перепишите пример из теории, но для положительного числа проверяйте —
+# четное оно или нет. Таки образом после проверок переменная result должна
+# содержать одно из четырех значений:
+#
+# "Positive even number"
+# "Positive odd number"
+# "Negative number"
+# "It is zero"
+# Подсказка: проверка на четность выполняется сравнением остатка от деления
+# на 2 с 0 или 1. Напомним, остаток от деления можно получить после операции %
+
+num = int(input("Enter a number: "))
+
+if
+    if
+        result = "Positive odd number"
+    else:
+        result = "Positive even number"
+elif
+    result = "Negative number"
+else:
+    result = "It is zero"
--- lesson2/task1.py
+++ lesson2/task1.py
@@ -3,7 +3,11 @@
 # Реалізуйте оператор контролю виконання так, щоб він привласнив логічній змінній is_next
 # значення True, якщо кількість набраних балів буде більшою або дорівнює 83. В іншому
 # випадку значення змінної дорівнює False
+from datetime import datetime
 
+
+date = datetime(year=2003, month=12, day=16)
+print(date.weekday())
 num = int(input("Enter the number of points: "))
 
 if num >= 83:
